c145322cfbb3d4c809333206a8835eeb1a1b4d49
Mich <anamichelle1308@gmail.com>
2023-11-22 08:30:32 -0600

    :sparkles: Hola mundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SaludoComponent } from './saludo/saludo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SaludoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mi-proyecto-hola-mundo';
